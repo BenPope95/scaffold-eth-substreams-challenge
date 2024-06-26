7f07590aef93785510ced707a00107198a55bee9
ayechan <ayechan.sant@gmail.com>
2024-06-26 15:22:58 -0700

    data displays well and styling

--- packages/nextjs/app/page.tsx
+++ packages/nextjs/app/page.tsx
@@ -4,6 +4,7 @@ import Link from "next/link";
 import { ApolloClient, ApolloProvider, InMemoryCache, gql, useQuery } from "@apollo/client";
 import type { NextPage } from "next";
 import { BugAntIcon, MagnifyingGlassIcon } from "@heroicons/react/24/outline";
+import Content from "~~/components/Content";
 
 const Home: NextPage = () => {
   const client = new ApolloClient({
@@ -11,31 +12,11 @@ const Home: NextPage = () => {
     cache: new InMemoryCache(),
   });
 
-  const query = gql`
-    query testQuery {
-      factories(first: 5) {
-        id
-        poolCount
-        txCount
-        totalVolumeUSD
-      }
-      bundles(first: 5) {
-        id
-        ethPriceUSD
-      }
-    }
-  `;
-
-  const { loading, error, data } = useQuery(query);
-
-  if (loading) return <p>Loading...</p>;
-  if (error) return <p>Error : {error.message}</p>;
-
   return (
     <ApolloProvider client={client}>
       <div className="flex items-center flex-col flex-grow pt-10">
         <div>Welcome to Substreams Challenge 1</div>
-        <div>{data}</div>
+        <Content />
       </div>
     </ApolloProvider>
   );
