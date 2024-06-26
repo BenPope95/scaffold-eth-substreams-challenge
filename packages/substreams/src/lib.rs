1dc303001ae9b2e2c8dadafb2b32e87880274b01
ayechan <ayechan.sant@gmail.com>
2024-06-26 13:02:10 -0700

    updating makefile

--- packages/substreams/src/lib.rs
+++ packages/substreams/src/lib.rs
@@ -20,100 +20,130 @@ substreams_ethereum::init!();
 const BAYC_TRACKED_CONTRACT: [u8; 20] = hex!("bc4ca0eda7647a8ab7c2061c2e118a18a936f13d");
 
 fn map_bayc_events(blk: &eth::Block, events: &mut contract::Events) {
-    events.bayc_approvals.append(&mut blk
-        .receipts()
-        .flat_map(|view| {
-            view.receipt.logs.iter()
-                .filter(|log| log.address == BAYC_TRACKED_CONTRACT)
-                .filter_map(|log| {
-                    if let Some(event) = abi::bayc_contract::events::Approval::match_and_decode(log) {
-                        return Some(contract::BaycApproval {
-                            evt_tx_hash: Hex(&view.transaction.hash).to_string(),
-                            evt_index: log.block_index,
-                            evt_block_time: Some(blk.timestamp().to_owned()),
-                            evt_block_number: blk.number,
-                            approved: event.approved,
-                            owner: event.owner,
-                            token_id: event.token_id.to_string(),
-                        });
-                    }
+    events.bayc_approvals.append(
+        &mut blk
+            .receipts()
+            .flat_map(|view| {
+                view.receipt
+                    .logs
+                    .iter()
+                    .filter(|log| log.address == BAYC_TRACKED_CONTRACT)
+                    .filter_map(|log| {
+                        if let Some(event) =
+                            abi::bayc_contract::events::Approval::match_and_decode(log)
+                        {
+                            return Some(contract::BaycApproval {
+                                evt_tx_hash: Hex(&view.transaction.hash).to_string(),
+                                evt_index: log.block_index,
+                                evt_block_time: Some(blk.timestamp().to_owned()),
+                                evt_block_number: blk.number,
+                                approved: event.approved,
+                                owner: event.owner,
+                                token_id: event.token_id.to_string(),
+                            });
+                        }
 
-                    None
-                })
-        })
-        .collect());
-    events.bayc_approval_for_alls.append(&mut blk
-        .receipts()
-        .flat_map(|view| {
-            view.receipt.logs.iter()
-                .filter(|log| log.address == BAYC_TRACKED_CONTRACT)
-                .filter_map(|log| {
-                    if let Some(event) = abi::bayc_contract::events::ApprovalForAll::match_and_decode(log) {
-                        return Some(contract::BaycApprovalForAll {
-                            evt_tx_hash: Hex(&view.transaction.hash).to_string(),
-                            evt_index: log.block_index,
-                            evt_block_time: Some(blk.timestamp().to_owned()),
-                            evt_block_number: blk.number,
-                            approved: event.approved,
-                            operator: event.operator,
-                            owner: event.owner,
-                        });
-                    }
+                        None
+                    })
+            })
+            .collect(),
+    );
+    events.bayc_approval_for_alls.append(
+        &mut blk
+            .receipts()
+            .flat_map(|view| {
+                view.receipt
+                    .logs
+                    .iter()
+                    .filter(|log| log.address == BAYC_TRACKED_CONTRACT)
+                    .filter_map(|log| {
+                        if let Some(event) =
+                            abi::bayc_contract::events::ApprovalForAll::match_and_decode(log)
+                        {
+                            return Some(contract::BaycApprovalForAll {
+                                evt_tx_hash: Hex(&view.transaction.hash).to_string(),
+                                evt_index: log.block_index,
+                                evt_block_time: Some(blk.timestamp().to_owned()),
+                                evt_block_number: blk.number,
+                                approved: event.approved,
+                                operator: event.operator,
+                                owner: event.owner,
+                            });
+                        }
 
-                    None
-                })
-        })
-        .collect());
-    events.bayc_ownership_transferreds.append(&mut blk
-        .receipts()
-        .flat_map(|view| {
-            view.receipt.logs.iter()
-                .filter(|log| log.address == BAYC_TRACKED_CONTRACT)
-                .filter_map(|log| {
-                    if let Some(event) = abi::bayc_contract::events::OwnershipTransferred::match_and_decode(log) {
-                        return Some(contract::BaycOwnershipTransferred {
-                            evt_tx_hash: Hex(&view.transaction.hash).to_string(),
-                            evt_index: log.block_index,
-                            evt_block_time: Some(blk.timestamp().to_owned()),
-                            evt_block_number: blk.number,
-                            new_owner: event.new_owner,
-                            previous_owner: event.previous_owner,
-                        });
-                    }
+                        None
+                    })
+            })
+            .collect(),
+    );
+    events.bayc_ownership_transferreds.append(
+        &mut blk
+            .receipts()
+            .flat_map(|view| {
+                view.receipt
+                    .logs
+                    .iter()
+                    .filter(|log| log.address == BAYC_TRACKED_CONTRACT)
+                    .filter_map(|log| {
+                        if let Some(event) =
+                            abi::bayc_contract::events::OwnershipTransferred::match_and_decode(log)
+                        {
+                            return Some(contract::BaycOwnershipTransferred {
+                                evt_tx_hash: Hex(&view.transaction.hash).to_string(),
+                                evt_index: log.block_index,
+                                evt_block_time: Some(blk.timestamp().to_owned()),
+                                evt_block_number: blk.number,
+                                new_owner: event.new_owner,
+                                previous_owner: event.previous_owner,
+                            });
+                        }
 
-                    None
-                })
-        })
-        .collect());
-    events.bayc_transfers.append(&mut blk
-        .receipts()
-        .flat_map(|view| {
-            view.receipt.logs.iter()
-                .filter(|log| log.address == BAYC_TRACKED_CONTRACT)
-                .filter_map(|log| {
-                    if let Some(event) = abi::bayc_contract::events::Transfer::match_and_decode(log) {
-                        return Some(contract::BaycTransfer {
-                            evt_tx_hash: Hex(&view.transaction.hash).to_string(),
-                            evt_index: log.block_index,
-                            evt_block_time: Some(blk.timestamp().to_owned()),
-                            evt_block_number: blk.number,
-                            from: event.from,
-                            to: event.to,
-                            token_id: event.token_id.to_string(),
-                        });
-                    }
+                        None
+                    })
+            })
+            .collect(),
+    );
+    events.bayc_transfers.append(
+        &mut blk
+            .receipts()
+            .flat_map(|view| {
+                view.receipt
+                    .logs
+                    .iter()
+                    .filter(|log| log.address == BAYC_TRACKED_CONTRACT)
+                    .filter_map(|log| {
+                        if let Some(event) =
+                            abi::bayc_contract::events::Transfer::match_and_decode(log)
+                        {
+                            return Some(contract::BaycTransfer {
+                                evt_tx_hash: Hex(&view.transaction.hash).to_string(),
+                                evt_index: log.block_index,
+                                evt_block_time: Some(blk.timestamp().to_owned()),
+                                evt_block_number: blk.number,
+                                from: event.from,
+                                to: event.to,
+                                token_id: event.token_id.to_string(),
+                            });
+                        }
 
-                    None
-                })
-        })
-        .collect());
+                        None
+                    })
+            })
+            .collect(),
+    );
 }
 
 fn db_bayc_out(events: &contract::Events, tables: &mut DatabaseChangeTables) {
     // Loop over all the abis events to create table changes
     events.bayc_approvals.iter().for_each(|evt| {
         tables
-            .create_row("bayc_approval", [("evt_tx_hash", evt.evt_tx_hash.to_string()),("evt_index", evt.evt_index.to_string())])
+            .create_row(
+                "bayc_approval",
+                [
+                    ("evt_tx_hash", evt.evt_tx_hash.to_string()),
+                    ("evt_index", evt.evt_index.to_string()),
+                ],
+            )
             .set("evt_block_time", evt.evt_block_time.as_ref().unwrap())
             .set("evt_block_number", evt.evt_block_number)
             .set("approved", Hex(&evt.approved).to_string())
@@ -122,7 +152,13 @@ fn db_bayc_out(events: &contract::Events, tables: &mut DatabaseChangeTables) {
     });
     events.bayc_approval_for_alls.iter().for_each(|evt| {
         tables
-            .create_row("bayc_approval_for_all", [("evt_tx_hash", evt.evt_tx_hash.to_string()),("evt_index", evt.evt_index.to_string())])
+            .create_row(
+                "bayc_approval_for_all",
+                [
+                    ("evt_tx_hash", evt.evt_tx_hash.to_string()),
+                    ("evt_index", evt.evt_index.to_string()),
+                ],
+            )
             .set("evt_block_time", evt.evt_block_time.as_ref().unwrap())
             .set("evt_block_number", evt.evt_block_number)
             .set("approved", evt.approved)
@@ -131,7 +167,13 @@ fn db_bayc_out(events: &contract::Events, tables: &mut DatabaseChangeTables) {
     });
     events.bayc_ownership_transferreds.iter().for_each(|evt| {
         tables
-            .create_row("bayc_ownership_transferred", [("evt_tx_hash", evt.evt_tx_hash.to_string()),("evt_index", evt.evt_index.to_string())])
+            .create_row(
+                "bayc_ownership_transferred",
+                [
+                    ("evt_tx_hash", evt.evt_tx_hash.to_string()),
+                    ("evt_index", evt.evt_index.to_string()),
+                ],
+            )
             .set("evt_block_time", evt.evt_block_time.as_ref().unwrap())
             .set("evt_block_number", evt.evt_block_number)
             .set("new_owner", Hex(&evt.new_owner).to_string())
@@ -139,7 +181,13 @@ fn db_bayc_out(events: &contract::Events, tables: &mut DatabaseChangeTables) {
     });
     events.bayc_transfers.iter().for_each(|evt| {
         tables
-            .create_row("bayc_transfer", [("evt_tx_hash", evt.evt_tx_hash.to_string()),("evt_index", evt.evt_index.to_string())])
+            .create_row(
+                "bayc_transfer",
+                [
+                    ("evt_tx_hash", evt.evt_tx_hash.to_string()),
+                    ("evt_index", evt.evt_index.to_string()),
+                ],
+            )
             .set("evt_block_time", evt.evt_block_time.as_ref().unwrap())
             .set("evt_block_number", evt.evt_block_number)
             .set("from", Hex(&evt.from).to_string())
@@ -148,12 +196,14 @@ fn db_bayc_out(events: &contract::Events, tables: &mut DatabaseChangeTables) {
     });
 }
 
-
 fn graph_bayc_out(events: &contract::Events, tables: &mut EntityChangesTables) {
     // Loop over all the abis events to create table changes
     events.bayc_approvals.iter().for_each(|evt| {
         tables
-            .create_row("bayc_approval", format!("{}-{}", evt.evt_tx_hash, evt.evt_index))
+            .create_row(
+                "bayc_approval",
+                format!("{}-{}", evt.evt_tx_hash, evt.evt_index),
+            )
             .set("evt_tx_hash", &evt.evt_tx_hash)
             .set("evt_index", evt.evt_index)
             .set("evt_block_time", evt.evt_block_time.as_ref().unwrap())
@@ -164,7 +214,10 @@ fn graph_bayc_out(events: &contract::Events, tables: &mut EntityChangesTables) {
     });
     events.bayc_approval_for_alls.iter().for_each(|evt| {
         tables
-            .create_row("bayc_approval_for_all", format!("{}-{}", evt.evt_tx_hash, evt.evt_index))
+            .create_row(
+                "bayc_approval_for_all",
+                format!("{}-{}", evt.evt_tx_hash, evt.evt_index),
+            )
             .set("evt_tx_hash", &evt.evt_tx_hash)
             .set("evt_index", evt.evt_index)
             .set("evt_block_time", evt.evt_block_time.as_ref().unwrap())
@@ -175,7 +228,10 @@ fn graph_bayc_out(events: &contract::Events, tables: &mut EntityChangesTables) {
     });
     events.bayc_ownership_transferreds.iter().for_each(|evt| {
         tables
-            .create_row("bayc_ownership_transferred", format!("{}-{}", evt.evt_tx_hash, evt.evt_index))
+            .create_row(
+                "bayc_ownership_transferred",
+                format!("{}-{}", evt.evt_tx_hash, evt.evt_index),
+            )
             .set("evt_tx_hash", &evt.evt_tx_hash)
             .set("evt_index", evt.evt_index)
             .set("evt_block_time", evt.evt_block_time.as_ref().unwrap())
@@ -185,7 +241,10 @@ fn graph_bayc_out(events: &contract::Events, tables: &mut EntityChangesTables) {
     });
     events.bayc_transfers.iter().for_each(|evt| {
         tables
-            .create_row("bayc_transfer", format!("{}-{}", evt.evt_tx_hash, evt.evt_index))
+            .create_row(
+                "bayc_transfer",
+                format!("{}-{}", evt.evt_tx_hash, evt.evt_index),
+            )
             .set("evt_tx_hash", &evt.evt_tx_hash)
             .set("evt_index", evt.evt_index)
             .set("evt_block_time", evt.evt_block_time.as_ref().unwrap())
